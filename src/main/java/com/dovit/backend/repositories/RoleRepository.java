7a728e3282c580ac0e9d782a00a342d9d6416a35
Ramon Paris <pariis78@gmail.com>
2019-12-08 11:29:30 -0300

    fix roles en servicios de LDAP

--- src/main/java/com/dovit/backend/repositories/RoleRepository.java
+++ src/main/java/com/dovit/backend/repositories/RoleRepository.java
@@ -1,11 +1,17 @@
 package com.dovit.backend.repositories;
 
 import com.dovit.backend.domain.Role;
+import com.dovit.backend.util.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * @author Ramón París
  * @since 02-10-2019
  */
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    List<Role> findAllByNameIn(List<RoleName> names);
+
 }
--- src/main/java/com/dovit/backend/services/AuthServiceImpl.java
+++ src/main/java/com/dovit/backend/services/AuthServiceImpl.java
@@ -4,16 +4,19 @@ import com.dovit.backend.domain.Company;
 import com.dovit.backend.domain.Role;
 import com.dovit.backend.domain.User;
 import com.dovit.backend.exceptions.BadRequestException;
+import com.dovit.backend.exceptions.CustomAccessDeniedException;
 import com.dovit.backend.model.requests.AuthRequest;
 import com.dovit.backend.model.requests.RegisterTokenRequest;
 import com.dovit.backend.model.requests.SignUpRequest;
 import com.dovit.backend.model.responses.AuthResponse;
+import com.dovit.backend.repositories.RoleRepository;
 import com.dovit.backend.repositories.UserRepository;
 import com.dovit.backend.security.CustomLdapUserDetails;
 import com.dovit.backend.security.CustomUserDetailsService;
 import com.dovit.backend.security.JwtTokenProvider;
 import com.dovit.backend.security.UserPrincipal;
 import com.dovit.backend.util.Constants;
+import com.dovit.backend.util.RoleName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,6 +27,8 @@ import org.springframework.security.ldap.userdetails.LdapUserDetailsImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -54,6 +59,9 @@ public class AuthServiceImpl implements AuthService {
     @Autowired
     private AuditService auditService;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Override
     public AuthResponse authenticateUser(AuthRequest request) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
@@ -68,8 +76,15 @@ public class AuthServiceImpl implements AuthService {
         } else { //If the authentication was made by ldap service
             CustomLdapUserDetails userPrincipal =  (CustomLdapUserDetails) authentication.getPrincipal();
             String jwt = tokenProvider.generateAuthToken(userPrincipal);
-            AuthResponse response = new AuthResponse(jwt, userPrincipal.getFirstName(), userPrincipal.getLastName(), authentication.getAuthorities().stream().map(Object::toString).collect(Collectors.joining(", ")), Constants.CLEVER_IT, 0L);
-            auditService.registerAudit(response, "INICIO DE SESIÓN", "OK", 0L);
+            String rolesString = authentication.getAuthorities().stream().map(Object::toString).collect(Collectors.joining(", "));
+            List<RoleName> roles = Arrays.stream(RoleName.values()).filter(r -> rolesString.contains(r.name())).collect(Collectors.toList());
+
+            if (roleRepository.findAllByNameIn(roles).size() == 0){
+                throw new CustomAccessDeniedException("User doesn't have a valid role");
+            }
+
+            AuthResponse response = new AuthResponse(jwt, userPrincipal.getFirstName(), userPrincipal.getLastName(), rolesString, Constants.CLEVER_IT, 0L);
+            auditService.registerAudit(response, "INICIO DE SESIÓN", "OK", null);
             return response;
         }
     }
